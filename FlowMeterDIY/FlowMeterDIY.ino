2c4dbbca658f71842a7fec469f588304a0590692
alexr <alexey.rykhalskiy@gmail.com>
2018-06-04 23:31:27 +0300

    debug ideas

--- FlowMeterDIY/FlowMeterDIY.ino
+++ FlowMeterDIY/FlowMeterDIY.ino
@@ -1,75 +1,47 @@
-byte statusLed       = 13;
+byte sensorPin       = 3; //18, 19, 20, 21; 2 and 3 don't work properly while LCD shield connected
+const float factor = 4.5; // 4.5 pulses per second per litre/minute of flow. 
 
-byte sensorInterrupt = 0;  // 0 = digital pin 2
-byte sensorPin       = 2;
-
-// The hall-effect flow sensor outputs approximately 
-float calibrationFactor = 4.5; // 4.5 pulses per second per litre/minute of flow. 
-// 270 pulses per minute 1L
-// 540 pulses per minute 2L
-// 2700 pulses per minute 10L
-// 5400 pulses per minute 20L
-// 8100 pulses per minute 30L
-// 10800 pulses per minute 40L
-// 13500 pulses per minute 50L
-// 16200 pulses per minute 60L
-
-volatile byte pulseCount;  
-float flowRate;
+volatile int pulseCount;  
+float flow;
 unsigned int flowMilliLitres;
 unsigned long totalMilliLitres;
 unsigned long oldTime;
 
 void setup(){
   Serial.begin(38400);
-  pinMode(statusLed, OUTPUT);
-  digitalWrite(statusLed, HIGH);  // We have an active-low LED attached
-  
   pinMode(sensorPin, INPUT);
   digitalWrite(sensorPin, HIGH);
 
   pulseCount        = 0;
-  flowRate          = 0.0;
+  flow              = 0.0;
   flowMilliLitres   = 0;
   totalMilliLitres  = 0;
   oldTime           = 0;
 
-  attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+  attachInterrupt(digitalPinToInterrupt(sensorPin), pulseCounter, FALLING);
+}
+
+String toReadable(float val) {
+  int roun = int(val);
+  int frac = (val - int(val)) * 10;
+  return String(roun)+"."+String(frac);
 }
 
 void loop() {
-   if((millis() - oldTime) > 1000) {   // Only process counters once per second
-    detachInterrupt(sensorInterrupt);
-    flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
-    oldTime = millis();
-    flowMilliLitres = (flowRate / 60) * 1000;
-    totalMilliLitres += flowMilliLitres;
-
-    unsigned int frac;
-    
-    // Print the flow rate for this second in litres / minute
-    Serial.print("Flow rate: ");
-    Serial.print(int(flowRate));  // Print the integer part of the variable
-    Serial.print(".");             // Print the decimal point
-
-    // Determine the fractional part. The 10 multiplier gives us 1 decimal place.
-    frac = (flowRate - int(flowRate)) * 10;
-    Serial.print(frac, DEC) ;      // Print the fractional part of the variable
-    Serial.print("L/min");
-    
-    // Print the number of litres flowed in this second
-    Serial.print("  Current Liquid Flowing: ");             // Output separator
-    Serial.print(flowMilliLitres);
-    Serial.print("mL/Sec");
-
-    // Print the cumulative total of litres flowed since starting
-    Serial.print("  Output Liquid Quantity: ");             // Output separator
-    Serial.print(totalMilliLitres);
-    Serial.println("mL"); 
-
-    pulseCount = 0;
-    attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
-  }
+    long current = millis();
+    if((current - oldTime) > 1000) {
+        detachInterrupt(digitalPinToInterrupt(sensorPin));
+        float quantTime = 1000.0 / (current - oldTime);
+        flow = quantTime * pulseCount / factor; // L/min
+        
+        //flowMilliLitres = (flow / 60) * 1000;
+        //totalMilliLitres += flowMilliLitres;
+ 
+        Serial.println("Flow rate: "+toReadable(flow)+" L/min");
+        oldTime = current;
+        pulseCount = 0;
+        attachInterrupt(digitalPinToInterrupt(sensorPin), pulseCounter, FALLING);
+    }
 }
 
 void pulseCounter() {
